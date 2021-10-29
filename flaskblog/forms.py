6c2eef7a589037457bbf1ae84ad730bd098d15ef
Sam Sonter <samuel.aarga@gmail.com>
2021-10-29 11:39:48 +0800

    fixed 404

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -1,32 +1,35 @@
-from flask_wtf import FlaskForm    
-from wtforms import StringField , PasswordField, SubmitField, BooleanField    
-from wtforms.validators import DataRequired, Email, EqualTo, Length, EqualTo , ValidationError
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from flaskblog.models import User
 
 
- 
-# validator is used to check if the field is empty or not
-# string field is used for form step up
-#Flask-WTF is a Flask extension that provides a simple interface for creating WTForms.
-
-
-# RegistrationForm is a class that inherits from FlaskForm
 class RegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[
-                           DataRequired(), Length(min=3, max=25)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired(),])
-    comfirm_password = PasswordField('comfirm_Password', validators=[DataRequired(),EqualTo('password')]) 
-    # EqualTo is used to check if the password is the same as the comfirm_password
-    submit = SubmitField('Sign up')
-    
-#class representing login form
-class LoginForm(FlaskForm):
-    email = StringField('email', validators=[DataRequired(), Email()])
-    password = PasswordField('password')
-    remember = BooleanField('remember me')
-    login = SubmitField('Login')
-    
-    
-    
-    
+    username = StringField('Username',
+                           validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Sign Up')
 
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError(
+                'That username is taken. Please choose a different one.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,9 @@
-
-from flaskblog import app
-from flask import  render_template, url_for, flash, redirect
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog import app, db, bcrypt
 from flaskblog.forms import RegistrationForm, LoginForm
-from flaskblog.models import User, Posts
+from flaskblog.models import User, Post
+from flask_login import login_user, current_user, logout_user, login_required
+
 
 posts = [
     {
@@ -12,22 +13,16 @@ posts = [
         'date_posted': 'April 20, 2018'
     },
     {
-        'author': 'mel d sol',
+        'author': 'melly mel',
         'title': 'Blog Post 2',
         'content': 'Second post content',
         'date_posted': 'April 21, 2018'
     },
     {
-        'author': 'dwayne johnson',
-        'title': 'Blog Post 3',
-        'content': 'Third post content',
-        'date_posted': 'April 22, 2018'
-    },
-    {
-        'author': 'aarga shemenge',
+        'author': 'drink water',
         'title': 'Blog Post 1',
         'content': 'Second post content',
-        'date_posted': 'March 11, 2019'
+        'date_posted': 'April 30, 2019'
     }
 ]
 
@@ -45,23 +40,44 @@ def about():
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(
+            form.password.data).decode('utf-8')
+        user = User(username=form.username.data,
+                    email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash('Your account has been created! You are now able to log in', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        if form.email.data == 'admin@blog.com' and form.password.data == 'password':
-            flash('You have been logged in!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if user and bcrypt.check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
-            flash('Login Unsuccessful. Please check username and password', 'danger')
+            flash('Login Unsuccessful. Please check email and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
 
+@app.route("/logout")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
 
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html', title='Account')
