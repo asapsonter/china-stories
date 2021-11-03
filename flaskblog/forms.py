6ccbb5fa397e72e70a4288f1c1d6ad67af4eb9ad
Sam Sonter <samuel.aarga@gmail.com>
2021-11-04 03:22:50 +0800

    bug fix

--- flaskblog/forms.py
+++ flaskblog/forms.py
@@ -15,13 +15,11 @@ class RegistrationForm(FlaskForm):
     submit = SubmitField('Sign Up')
 
     def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose a different one.')
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('That username is taken. Please choose a different one.')
 
     def validate_email(self, email):
-        if email.data != current_user.email:
             user = User.query.filter_by(email=email.data).first()
             if user:
                 raise ValidationError('That email is taken. Please choose a different one.')
